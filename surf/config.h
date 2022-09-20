edd4d487f6d590ab28cd30d16be8fa662b87ef08
mekb the turtle <mekbgithub@icloud.com>
2022-09-20 10:49:49 +1000

    add surf f5 keybind

--- surf/config.h
+++ surf/config.h
@@ -138,6 +138,7 @@ static Key keys[] = {
 	{ 0,                     GDK_KEY_Escape, stop,       { 0 } },
 	{ MODKEY,                GDK_KEY_c,      stop,       { 0 } },
 
+	{ 0,                     GDK_KEY_F5,     reload,     { .i = 1 } },
 	{ MODKEY|GDK_SHIFT_MASK, GDK_KEY_r,      reload,     { .i = 1 } },
 	{ MODKEY,                GDK_KEY_r,      reload,     { .i = 0 } },
 
